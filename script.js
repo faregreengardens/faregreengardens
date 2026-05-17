d04422e1db5056a953e4176e1c7e18e5ea7c645e
Andy <sgtandy100@gmail.com>
2026-05-17 12:38:13 -0400

    updated website

--- script.js
+++ script.js
@@ -2,20 +2,39 @@ const slider = document.getElementById("slider");
 
 let currentSlide = 0;
 
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
 function moveSlide(direction) {
 
-  const boxes = document.querySelectorAll(".box");
+  const sliderWrapper =
+    document.querySelector(".slider-wrapper");
 
-  const boxWidth = 280; // box width + gap
+  const boxes =
+    document.querySelectorAll(".box");
+
+  const boxWidth =
+    boxes[0].offsetWidth +
+    parseInt(getComputedStyle(slider).gap);
 
   currentSlide += direction;
+
+  const maxSlide =
+    Math.ceil(
+      (slider.scrollWidth - sliderWrapper.offsetWidth)
+      / boxWidth
+    );
 
   if (currentSlide < 0) {
     currentSlide = 0;
   }
 
-  if (currentSlide > boxes.length - 2) {
-    currentSlide = boxes.length - 2;
+  if (currentSlide > maxSlide) {
+    currentSlide = maxSlide;
   }
 
   slider.style.transform =
